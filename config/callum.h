efe7bd4dfd84032e6436cd6d9920b962f5206f8d
KaJIH <kajih@gramr>
2025-12-13 22:05:05 +0100

    reworked mod locks

--- config/callum.h
+++ config/callum.h
@@ -1,16 +1,16 @@
 #pragma once
 
-#define QWE  0
-#define QML  1
-#define QMR  2
-#define COL  3
-#define CML  4
-#define CMR  5
-#define NUM  6
-#define SYM  7
-#define FUN  8
-#define BTN  9
-#define TRI  10
-#define NAV  11
-#define ADJ  12
-
+#define QWE 0
+#define QML 1
+#define QMR 2
+#define COL 3
+#define CML 4
+#define CMR 5
+#define NUM 6
+#define SYM 7
+#define FUN 8
+#define BTN 9
+#define TRI 10
+#define NAV 11
+#define ADJ 12
+#define LOCK 13
